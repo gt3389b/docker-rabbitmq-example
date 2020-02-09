c864ec78e38225f0866b204c90b1a6d9dc6e124f
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2020-02-09 09:15:18 +0100

    :sparkles: Updated consumer

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import os
+import json
 import pika
 import time
 
 amqp_url = os.environ['AMQP_URL']
-print('URL: %s' % (amqp_url,))
+print(f'URL: {amqp_url}')
 parameters = pika.URLParameters(amqp_url)
 print(parameters)
 connection = pika.BlockingConnection(parameters)
@@ -12,8 +13,21 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+def callback(channel, method, properties, body):
+    print(f"channel {channel}")
+    print(f"method {method}")
+    print(f"properties {properties}")
+    # print("properties {0}".format(dir(properties)))
+    list_properties = \
+        ('app_id', 'cluster_id', 'content_encoding', 'content_type',
+         'correlation_id', 'decode', 'delivery_mode', 'encode',
+         'expiration', 'headers', 'message_id', 'priority',
+         'reply_to', 'timestamp', 'type', 'user_id')
+    for k in list_properties:
+        print("properties {0} : {1}".format(k, getattr(properties, k)))
+
+    print(" [x] Received : {0}".format(str(json.loads(body))))
+
 
 channel.basic_consume(
     queue='hello', on_message_callback=callback, auto_ack=True)

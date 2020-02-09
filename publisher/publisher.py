9dda18994844316d88c5398965aa74b82185c503
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2020-02-09 09:15:30 +0100

    :sparkles: Updated publisher

--- publisher/publisher.py
+++ publisher/publisher.py
@@ -3,7 +3,7 @@ import os
 import pika
 
 amqp_url = os.environ['AMQP_URL']
-print('URL: %s' % (amqp_url,))
+print(f'URL: {amqp_url}')
 parameters = pika.URLParameters(amqp_url)
 print(parameters)
 connection = pika.BlockingConnection(parameters)
@@ -11,6 +11,12 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+channel.basic_publish(exchange='', routing_key='hello',
+                      properties=pika.BasicProperties(
+                            content_type='application/json',
+                            message_id='42',
+                            user_id='13',
+                            correlation_id='666'),
+                      body='{"payload": "Hello World!"}')
 print(" [x] Sent 'Hello World!'")
 connection.close()
